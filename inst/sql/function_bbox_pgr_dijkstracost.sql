79c514f4e3d6d4a62a8b6bb0d27f865de6ad4cde
Marcus Young <m.a.young@soton.ac.uk>
2018-12-08 00:53:31 +0000

    sql functions

---
+++ inst/sql/function_bbox_pgr_dijkstracost.sql
@@ -0,0 +1,26 @@
+-- expanded version of pgr_dijkstracost to include bounding box and pgr_pointtoedgenode
+create
+	or replace function openroads.bbox_pgr_dijkstracost ( sql text, start_pt geometry, end_pt geometry, directed boolean, tol_dist float8 default 100, expand_percent float4 default 0.5 ) returns table ( agg_cost float8 ) as $body$ declare
+	var_source_id bigint;
+var_target_id bigint;
+var_new_sql text;
+var_expand_geom geometry;
+begin
+		execute'create temp view edge as ' || sql || '; ';
+	var_source_id := pgr_pointtoedgenode ( 'edge', start_pt, tol_dist );
+	var_target_id := pgr_pointtoedgenode ( 'edge', end_pt, tol_dist );
+	drop view edge;
+	var_new_sql := sql;
+-- create a bbox that is expanded x percent
+-- around the bbox of start and end
+	var_expand_geom := st_expand ( st_makeline ( start_pt, end_pt ), st_distance ( start_pt, end_pt ) * expand_percent );
+-- only include edges that overlap the expand bounding box
+	var_new_sql := 'select * from (' || sql || ') as e
+	where e.the_geom &&  ' || quote_literal( var_expand_geom :: text ) || '::geometry';
+	return query select
+	d.agg_cost
+	from
+		pgr_dijkstracost ( var_new_sql, var_source_id, var_target_id, directed ) as d;
+
+end;
+$body$ language plpgsql
--- inst/sql/function_sdr_nearest_stations.sql
+++ inst/sql/function_sdr_nearest_stations.sql
@@ -7,8 +7,8 @@
 -- this version is withoutpoints.
 
 create or replace function sdr_nearest_stations(origin geometry, expand_pc float4 default 0.5) returns
-table(name text, crscode text, distance numeric)
-as $$
+table(name text, crscode text, distance float8)
+as $body$
 DECLARE
 	sa character VARYING;
 -- find the first service area where origin intersects 10 or more stations.
@@ -21,9 +21,11 @@ begin
 		when (select count (*) from data.stations where origin && service_area_20km) >= 10 then 'service_area_20km'
 		when (select count (*) from data.stations where origin && service_area_30km) >= 10 then 'service_area_30km'
 		when (select count (*) from data.stations where origin && service_area_40km) >= 10 then 'service_area_40km'
-		else 'service_area_67km' end
+		when (select count (*) from data.stations where origin && service_area_30km) >= 10 then 'service_area_60km'
+		when (select count (*) from data.stations where origin && service_area_40km) >= 10 then 'service_area_80km'
+		else 'service_area_105km' end
 	into sa;
-	return query execute format ('select name, crscode, round(r.agg_cost::numeric, 2) as distance from data.stations as d,
+	return query execute format ('select name, crscode, r.agg_cost, 2) as distance from data.stations as d,
 	lateral bbox_pgr_dijkstracost(
 		$sql$select id,
 		source,
@@ -42,4 +44,4 @@ begin
 	using origin, expand_pc;
 	return;
 	END
-	$$ language plpgsql;
+	$body$ language plpgsql;
