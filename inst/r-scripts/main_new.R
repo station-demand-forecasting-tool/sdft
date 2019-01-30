4d26a1497fb479891106674ad40d16db02dcfa72
Marcus Young <m.a.young@soton.ac.uk>
2019-01-30 17:11:48 +0000

    Complete abstraction analysis and  rationalisation of code

--- inst/r-scripts/main_new.R
+++ inst/r-scripts/main_new.R
@@ -195,16 +195,15 @@ getQuery(con, query)
 # probability weighted population
 
 # population column
-query <- paste0("
-                alter table model.exogenous_input add column population int8;
-                ")
+query <- paste0(
+  "
+  alter table model.exogenous_input
+  add column population int8,
+  add column avg_hhsize numeric;
+  "
+)
 getQuery(con, query)
 
-# average household size for the local authority where the postcode is located
-query <- paste0("
-                alter table model.exogenous_input add column avg_hhsize numeric;
-                ")
-getQuery(con, query)
 
 # copy from number to population for type 'population'
 query <- paste0("
@@ -277,11 +276,11 @@ sdr_create_service_areas(
 )
 
 
-# create probability table------------------------------------------------------
+# generate probability table----------------------------------------------------
 
 if (isolation) {
   # we only want to generate choice set once for each unique station
-  # as this is a long and processor intense function.
+  # as this is a long and processor intensive  function.
   # Multiple stations with the same choice set can be input to for sensitivity
   # testing
 
@@ -294,17 +293,9 @@ if (isolation) {
   for (name in unique_stations$name) {
     # get the first crscode for a station with name
     first_crs <- stations$crscode[stations$name == name][1]
+
     # generate the choiceset for that crs
     choicesets <- sdr_generate_choicesets(first_crs)
-
-    # remove rows for any postcode where the proposed station is not
-    # in the choice set
-
-    choicesets <- choicesets %>%
-      group_by(postcode) %>%
-      filter(any(crscode == first_crs)) %>%
-      ungroup
-
 
     # for every crscode with the same station
     for (crscode in stations$crscode[stations$name == name]) {
@@ -312,93 +303,28 @@ if (isolation) {
       # crscode
       choicesets$crscode[choicesets$crscode == first_crs] <- crscode
 
+      sdr_generate_probability_table(choicesets, tolower(crscode))
 
-      # create probability table for this crscode
-      query <- paste0(
-        "create table model.probability_",
-        tolower(crscode),
-        "
-        (
-        id            serial primary key,
-        postcode      text,
-        crscode       text,
-        distance      double precision,
-        distance_rank smallint
-        );
-        "
-      )
-      getQuery(con, query)
+      # make frequency group adjustments if required
+      if (!is.na(stations$freqgrp[stations$crscode == crscode])) {
+        df <-
+          data.frame(fgrp = config[config$group_id == stations$freqgrp[stations$crscode == crscode],
+                                   "group_crs"], stringsAsFactors = FALSE)
+        sdr_frequency_group_adjustment(df, crscode)
 
-      # write the table for this crscode
-      dbWriteTable(
-        conn = con,
-        name = c('model', paste0("probability_",
-                                 tolower(crscode))),
-        choicesets,
-        append =
-          TRUE,
-        row.names = FALSE
-      )
+      } # end if freqgrp
+
+      # calculate the probabilities
+      sdr_calculate_probabilities(crscode)
+
     }
   }
 } else {
+  # Concurrent method
+
   choicesets <- sdr_generate_choicesets(stations$crscode)
 
-  # remove rows for any postcode where none of the proposed stations are in
-  # the choice set
-
-  choicesets <- choicesets %>%
-    group_by(postcode) %>%
-    filter(any(crscode %in% stations$crscode)) %>%
-    ungroup
-
-
-  query <- paste0(
-    "create table model.probability_concurrent
-    (
-    id            serial primary key,
-    postcode      text,
-    crscode       text,
-    distance      double precision,
-    distance_rank smallint
-    );
-    "
-  )
-  getQuery(con, query)
-
-  dbWriteTable(
-    conn = con,
-    name = c('model', 'probability_concurrent'),
-    choicesets,
-    append =
-      TRUE,
-    row.names = FALSE
-  )
-}
-
-# populate probability table(s)-------------------------------------------------
-
-
-if (isolation) {
-  for (crscode in stations$crscode) {
-    # populate probability table
-    sdr_populate_probability_table(crscode)
-
-    # make frequency group adjustments if required
-    if (!is.na(stations$freqgrp[stations$crscode == crscode])) {
-      df <-
-        data.frame(fgrp = config[config$group_id == stations$freqgrp[stations$crscode == crscode], "group_crs"], stringsAsFactors = FALSE)
-      sdr_frequency_group_adjustment(df, crscode)
-
-    } # end if freqgrp
-
-    # calculate probabilities
-    sdr_calculate_probabilities(crscode)
-
-  }
-} else {
-  # populate probability table
-  sdr_populate_probability_table("concurrent")
+  sdr_generate_probability_table(choicesets, "concurrent")
 
   # make frequency group adjustments if required
   # must only be a single frequency group for concurrent treatment
@@ -416,6 +342,7 @@ if (isolation) {
   sdr_calculate_probabilities("concurrent")
 
 }
+
 
 # trip end model----------------------------------------------------------------
 
@@ -483,17 +410,19 @@ for (crscode in stations$crscode) {
       sdr_calculate_prweighted_population(crscode, "concurrent")
 
   }
- # update model.proposed_stations table
+  # update model.proposed_stations table
 
-  query <- paste0("
-                  update model.proposed_stations set prw_pop = ", prweighted_pop,
-      " where crscode = '",
-                  crscode, "'")
+  query <- paste0(
+    "
+    update model.proposed_stations set prw_pop = ",
+    prweighted_pop,
+    " where crscode = '",
+    crscode,
+    "'"
+  )
   getQuery(con, query)
 
 }
-
-
 
 
 # Generate forecasts------------------------------------------------------------
@@ -577,7 +506,6 @@ getQuery(con, query)
 
 # regional-based uplift forecast
 
-
 query <- paste0(
   "
   with tmp as (
@@ -599,7 +527,7 @@ getQuery(con, query)
 # if unique value of abstract column is not a character then there are no abstraction stations
 # care needed, depends on the input file, assumes empty in this position of delimited file.
 
-if (!is.character(unique(test$abstract))) {
+if (!is.character(unique(stations$abstract))) {
   # For some components of the analysis we only need to consider unique stations
   # where abstraction analysis is required across all the proposed stations, so
   # lets get a vector of those
@@ -622,34 +550,22 @@ if (!is.character(unique(test$abstract))) {
     choicesets <-
       sdr_generate_choicesets(crscode, existing = TRUE)
 
-    # create before probability table for this crscode
-    query <- paste0(
-      "create table model.probability_before_abs_",
-      tolower(crscode),
-      "
-      (
-      id            serial primary key,
-      postcode      text,
-      crscode       text,
-      distance      double precision,
-      distance_rank smallint
-      );
-      "
-    )
-    getQuery(con, query)
+    # Generate probability table
+    sdr_generate_probability_table(choicesets, paste0(tolower(crscode), "_before_abs"))
 
-    # write the table for this crscode
-    dbWriteTable(
-      conn = con,
-      name = c('model', paste0("probability_before_abs_",
-                               tolower(crscode))),
-      choicesets,
-      append =
-        TRUE,
-      row.names = FALSE
-    )
+    # No frequency group adustments are needed for the before situation.
+    # Is this valid? I think so. Confirm.
 
-    # need to populate the before tables and calulcate probabilities and get weighted population.
+    # calculate probabilities
+    sdr_calculate_probabilities(paste0(tolower(crscode),
+                                       "_before_abs"))
+
+
+    # get probability weighted population
+
+    prweighted_pop <-
+      sdr_calculate_prweighted_population(crscode, paste0(tolower(crscode),
+                                                          "_before_abs"))
 
   }
 
@@ -665,62 +581,26 @@ if (!is.character(unique(test$abstract))) {
         choicesets <-
           sdr_generate_choicesets(crscode, existing = FALSE, abs_crs = abs_crscode)
 
-        # remove rows for any postcode where the abs_crscode is not
-        # in the choice set
 
-        choicesets <- choicesets %>%
-          group_by(postcode) %>%
-          filter(any(crscode == abs_crscode)) %>%
-          ungroup
-
-        # create after probability table for this abs_crscode:crscode
-        query <- paste0(
-          "create table model.probability_",
+        # Generate probability table
+        sdr_generate_probability_table(choicesets, paste0(
           tolower(abs_crscode),
           "_after_abs_",
-          tolower(crscode),
-          "
-          (
-          id            serial primary key,
-          postcode      text,
-          crscode       text,
-          distance      double precision,
-          distance_rank smallint
-          );
-          "
-        )
-        getQuery(con, query)
-
-        # write the table for this abs_crscode:crscode
-        dbWriteTable(
-          conn = con,
-          name = c('model', paste0(
-            "probability_",
-            tolower(abs_crscode),
-            "_after_abs_",
-            tolower(crscode)
-          )),
-          choicesets,
-          append =
-            TRUE,
-          row.names = FALSE
-        )
-
-        # populate probability table
-        sdr_populate_probability_table(paste0(
-          tolower(abs_crscode),
-          "_after_abs_",
-          tolower(crscode)))
-
+          tolower(crscode)
+        ))
 
         # make frequency group adjustments if required
         if (!is.na(stations$freqgrp[stations$crscode == crscode])) {
           df <-
-            data.frame(fgrp = config[config$group_id == stations$freqgrp[stations$crscode == crscode], "group_crs"], stringsAsFactors = FALSE)
+            data.frame(fgrp = config[config$group_id == stations$freqgrp[stations$crscode == crscode],
+                                     "group_crs"],
+                       stringsAsFactors = FALSE)
+
           sdr_frequency_group_adjustment(df, paste0(
             tolower(abs_crscode),
             "_after_abs_",
-            tolower(crscode)))
+            tolower(crscode)
+          ))
 
         } # end if freqgrp
 
@@ -729,104 +609,67 @@ if (!is.character(unique(test$abstract))) {
         sdr_calculate_probabilities(paste0(
           tolower(abs_crscode),
           "_after_abs_",
-          tolower(crscode)))
+          tolower(crscode)
+        ))
 
 
         # get probability weighted population
 
         prweighted_pop <-
-          sdr_calculate_prweighted_population(abs_crscode, paste0(
-            tolower(abs_crscode),
-            "_after_abs_",
-            tolower(crscode)))
-
-
+          sdr_calculate_prweighted_population(abs_crscode,
+                                              paste0(
+                                                tolower(abs_crscode),
+                                                "_after_abs_",
+                                                tolower(crscode)
+                                              ))
 
       }
     }
   } else {
-    # abstraction entry MUST be same for all proposed stations when submitted, so we
+    # Concurrent method
+
+    # Abstraction entry MUST be same for all proposed stations when submitted, so we
     # just need to get the entry for the first row and loop through each of the
     # requested abstraction analysis stations.
-    for (abs_crscode in unlist(strsplit(stations$abstract[1], ","))) {
 
+    for (abs_crscode in unlist(strsplit(stations$abstract[1], ","))) {
       # for each abs_crscode we pass all the proposed stations to the function
       # as all would be present in the after situation in concurrent mode
       choicesets <-
         sdr_generate_choicesets(stations$crscode,
-                                         existing = FALSE,
-                                         abs_crs = abs_crscode)
+                                existing = FALSE,
+                                abs_crs = abs_crscode)
 
-      # remove rows for any postcode where abs_crscode is not in
-      # the choice set
 
-      choicesets <- choicesets %>%
-        group_by(postcode) %>%
-        filter(any(crscode %in% abs_crscode)) %>%
-        ungroup
-
-      # create after probability table for this abs_crscode and all proposed stations
-      query <- paste0(
-        "create table model.probability_",
-        tolower(abs_crscode),
-        "_after_abs_concurrent
-        (
-        id            serial primary key,
-        postcode      text,
-        crscode       text,
-        distance      double precision,
-        distance_rank smallint
-        );
-        "
-      )
-      getQuery(con, query)
-
-      # write the probability table for this abs_crscode and all proposed stations
-      dbWriteTable(
-        conn = con,
-        name = c('model', paste0(
-          "probability_",
-          tolower(abs_crscode),
-          "_after_abs_concurrent"
-        )),
-        choicesets,
-        append =
-          TRUE,
-        row.names = FALSE
-      )
-
-      sdr_populate_probability_table(paste0(
-        tolower(abs_crscode),
-        "_after_abs_concurrent"))
+      # Generate probability table
+      sdr_generate_probability_table(choicesets, paste0(tolower(abs_crscode), "_after_abs_concurrent"))
 
 
       # make frequency group adjustments if required
-      # must only be a single identical frequency group for all stations under concurrent treatment
+      # must only be a single identical frequency group for all stations under
+      # concurrent treatment.
       # So we just check the first row for the group name and process once
 
       if (!is.na(stations$freqgrp[1])) {
         df <-
-          data.frame(fgrp = config[config$group_id == stations$freqgrp[1], "group_crs"], stringsAsFactors = FALSE)
+          data.frame(fgrp = config[config$group_id == stations$freqgrp[1],
+                                   "group_crs"],
+                     stringsAsFactors = FALSE)
 
-        sdr_frequency_group_adjustment(df, paste0(
-          tolower(abs_crscode),
-          "_after_abs_concurrent"))
+        sdr_frequency_group_adjustment(df, paste0(tolower(abs_crscode),
+                                                  "_after_abs_concurrent"))
 
       }
 
       # calculate probabilities
-      sdr_calculate_probabilities(paste0(
-        tolower(abs_crscode),
-        "_after_abs_concurrent"))
+      sdr_calculate_probabilities(paste0(tolower(abs_crscode),
+                                         "_after_abs_concurrent"))
 
       # get probability weighted population
 
       prweighted_pop <-
-        sdr_calculate_prweighted_population(abs_crscode, paste0(
-          tolower(abs_crscode),
-          "_after_abs_concurrent"))
-
-
+        sdr_calculate_prweighted_population(abs_crscode, paste0(tolower(abs_crscode),
+                                                                "_after_abs_concurrent"))
     }
   }
 } #end abstraction analysis
