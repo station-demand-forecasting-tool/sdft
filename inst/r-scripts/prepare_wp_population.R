f1eb879d7fbd55952258b3c0a6f50662c551e5b3
Marcus Young <marcus@graspit.co.uk>
2019-05-08 22:32:51 +0100

    New script to prepare workplace zone full dataset

--- inst/r-scripts/prepare_wp_population.R
+++ inst/r-scripts/prepare_wp_population.R
@@ -5,3 +5,27 @@ scottish_wp_population <- read.csv(file = "inst/source_data/workplace_zone_popul
 
 # write to database table
 
+dbWriteTable(
+  conn = con,
+  Id(schema = "data", table = "scottish_wp_population"),
+  scottish_wp_population,
+  append = FALSE,
+  row.names = FALSE
+)
+
+# join population to zone centroids
+
+# set up id as primary key
+query <- paste0("
+                alter table data.workplace_zones_scotland
+                add column population integer
+                ")
+sdr_dbExecute(con, query)
+
+query <- paste0("
+                update data.workplace_zones_scotland a
+set population = b.population
+from data.scottish_wp_population b
+WHERE a.wpz = b.id
+                ")
+sdr_dbExecute(con, query)
